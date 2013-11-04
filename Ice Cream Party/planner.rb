96e68ff280710aa0b4e43030dfed1d8af2edd9bb
shanghaiellen@gmail.com <shanghaiellen@gmail.com>
2013-11-04 15:53:22 -0800

    added the ability to input multiple flavors

--- Ice Cream Party/planner.rb
+++ Ice Cream Party/planner.rb
@@ -1,12 +1,12 @@
 def enter_flavors
-  puts "What's your name: "
+  puts "Who are you adding to the list?"
   input = gets.chomp
   if input == 'done'
     return input
   end
   name = input
-  puts 'Enter your favorite flavor: '
-  flavor = gets.chomp
+  puts 'Enter their favorite flavors, separated by a space: '
+  flavor = gets.chomp.split
   return name, flavor
 end
 
@@ -17,12 +17,13 @@ def make_flavor_hash
     name, flavor = enter_flavors
     flavor_hash[name] = flavor
   end
+  flavor_hash.delete('done')
   return flavor_hash
 end
 
 def which_report
   puts 'Which report would you like: by (g)uest or by (f)lavor: '
-  report = gets.chomp
+  report = gets.chomp.downcase
   if report == 'g'
     guest_by_flavor
   elsif report == 'f'
@@ -31,7 +32,7 @@ def which_report
 end
 
 def guest_by_flavor
-  @flavor_hash.each { |key, value| puts "#{key} prefers #{value}"}
+  @flavor_hash.each { |key, value| puts "#{key} prefers #{value.join(", ")}"}
 end
 
 def flavor_by_guest
@@ -39,15 +40,15 @@ def flavor_by_guest
   flavor_array = []
 
   @flavor_hash.each do |name, flavor|
-
-    if flavor_array.include?(flavor)
-      guest_hash[flavor].push(name)
-    else
-      guest_hash[flavor] = [name]
-      flavor_array.push(flavor)
+    flavor.each do |each_flavor|
+      if flavor_array.include?(each_flavor)
+        guest_hash[each_flavor].push(name)
+      else
+        guest_hash[each_flavor] = [name]
+        flavor_array.push(each_flavor)
+      end 
     end
   end
-  puts guest_hash
   guest_hash.keys.sort.each { |key| puts "#{key} is preferred by #{guest_hash[key].join(", ")}"}
 end
 
